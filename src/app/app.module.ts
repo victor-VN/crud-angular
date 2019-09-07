eeb45b28bdf774fd5679b0731fa12175352ad690
victor-VN <v1ctoroliveiranasc@gmail.com>
2019-09-06 21:16:48 -0300

    Creating components and services
    - Jogador Component
    Jogador Service
    - Jogador-criar Component
    - Jogador-Lista Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JogadorComponent } from './jogador/jogador.component';
+import { JogadorListaComponent } from './jogador-lista/jogador-lista.component';
+import { JogadorCriarComponent } from './jogador-criar/jogador-criar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JogadorComponent,
+    JogadorListaComponent,
+    JogadorCriarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/jogador-lista/jogador-lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jogador-lista',
+  templateUrl: './jogador-lista.component.html',
+  styleUrls: ['./jogador-lista.component.css']
+})
+export class JogadorListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
